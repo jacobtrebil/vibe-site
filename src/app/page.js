8e77d1c6d47993ce56a9b26c341fa1277c48a5cd
Jacob Trebil <jacobtrebil@yahoo.com>
2024-06-23 11:12:36 -0400

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import img from "../../src/app/IMG_7866.png";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <h1>Coming Soon</h1>
+        <h1>Prototype v1.0.0</h1>
+        <Image className="img" src={img} alt="Picture of the prototype" />
       </div>
     </main>
   );
