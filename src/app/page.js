c0e45b181a2bb862a9af33da418c900edacbbd55
Jacob Trebil <jacobtrebil@yahoo.com>
2024-10-27 17:58:47 +0100

    new:

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
 import Image from "next/image";
-import img from "../../src/app/IMG_7866.png";
+import img from "../../src/app/vibe.png";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <h1>Prototype v1.0.0</h1>
-        <Image className="img" src={img} alt="Picture of the prototype" />
+        <div className="headlineDiv">
+          <div className="blackBox"></div>
+          <h1 className="vibeHeadline">VIBE</h1>
+        </div>
+        <h2>AI Necklace With a Camera</h2>
+        <div>
+          <Image className="img" src={img} alt="Picture of the prototype" />
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="buttonDiv">
+          <button className="joinWaitlist">Join Waitlist</button>
+        </div>
       </div>
     </main>
   );
