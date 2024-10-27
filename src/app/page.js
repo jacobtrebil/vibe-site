af08626eda0f4cfbeeee3cc3b6e4ca286bb96eb2
Jacob Trebil <jacobtrebil@yahoo.com>
2024-10-27 20:16:58 +0100

    save

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import img from "../../src/app/vibe.png";
+import logo from "../../src/app/logo2.png";
 
 export default function Home() {
   return (
@@ -9,12 +10,10 @@ export default function Home() {
           <div className="blackBox"></div>
           <h1 className="vibeHeadline">VIBE</h1>
         </div>
-        <h2>AI Necklace With a Camera</h2>
-        <div>
+        <div className="mainDiv">
           <Image className="img" src={img} alt="Picture of the prototype" />
           <ul>
-            <li></li>
-            <li></li>
+            <li>Your Stylish AI Necklace That Tracks Your Habits <u>Passively</u> and Keeps You <u>Accountable</u></li>
           </ul>
         </div>
         <div className="buttonDiv">
@@ -24,3 +23,14 @@ export default function Home() {
     </main>
   );
 }
+
+/*  
+
+<Image className="logo" src={logo} alt="VIBE logo" width={100} style={{ margin: "0 auto" }} /> 
+
+<h2>Your <i>Stylish</i> AI Necklace <br></br>Habit Tracker</h2>
+
+<li>Get AI suggestions and assistance in real-time</li>
+<li>AI Assistant That Completes Tasks <u>For You</u></li>
+            
+*/
