b805504cb737f991b011a67b7101a57a3e7209fb
Jacob Trebil <jacobtrebil@yahoo.com>
2024-10-28 14:05:36 +0100

    save

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,38 @@
 import Image from "next/image";
 import img from "../../src/app/vibe.png";
 import logo from "../../src/app/logo2.png";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main>
+      <Script async data-uid="cb96ddd2c6" src="https://fresh-3.ck.page/cb96ddd2c6/index.js" />
       <div>
         <div className="headlineDiv">
           <div className="blackBox"></div>
           <h1 className="vibeHeadline">VIBE</h1>
+          <h2>Your AI Necklace + Camera<br></br>Habit Tracker & Coach</h2>
         </div>
         <div className="mainDiv">
           <Image className="img" src={img} alt="Picture of the prototype" />
-          <ul>
-            <li>Your Stylish AI Necklace That Tracks Your Habits <u>Passively</u> and Keeps You <u>Accountable</u></li>
-          </ul>
         </div>
         <div className="buttonDiv">
-          <button className="joinWaitlist">Join Waitlist</button>
+          <button className="joinWaitlist" data-formkit-toggle="cb96ddd2c6" src="https://fresh-3.ck.page/cb96ddd2c6/index.js">Join Waitlist</button>
         </div>
       </div>
+      <footer>
+        <div className="footerBox"></div>
+        <h1 className="footerText">© Vibe Forever | Built at Harvard & MIT</h1>
+      </footer>
     </main>
   );
 }
 
 /*  
+
+          <ul>
+            <li>Your Stylish AI Necklace That Tracks Your Habits <u>Passively</u> and Keeps You <u>Accountable</u></li>
+          </ul>
 
 <Image className="logo" src={logo} alt="VIBE logo" width={100} style={{ margin: "0 auto" }} /> 
 
